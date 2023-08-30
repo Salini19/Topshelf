c346cbffaf3bf0e266dd650f89fb2a0be35173ff
Salini <shalini021037@gmail.com>
2023-08-30 19:27:41 +0530

    Files added

---
+++ DotNet_Framework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowserviceWithTopshelf;
+
+namespace DotNet_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureService.Configure();
+        }
+    }
+}
